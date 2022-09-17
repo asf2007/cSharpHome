3cf8878f213fcf01e03005873ff290842b31a7d0
ASF <leha.flyagin@mail.ru>
2022-09-17 21:30:00 +0300

    homework5

---
+++ task34/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+int[] CreateNewArrRnd(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void ArrWriter(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        {
+            Console.Write($"{arr[i]},");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}");
+        }
+    }
+    Console.Write("]");
+}
+int CountParity(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+int[] array = CreateNewArrRnd(10, 100, 999);
+ArrWriter(array);
+Console.Write($" -> {CountParity(array)}");
---
+++ task38/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+double[] CreateNewArrRnd(int size)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * 100, 1);
+    }
+    return array;
+}
+void ArrWriter(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        {
+            Console.Write($"{arr[i]} | ");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}");
+        }
+    }
+    Console.Write("]");
+}
+double DifferenceMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return max - min;
+}
+double[] array = CreateNewArrRnd(6);
+ArrWriter(array);
+Console.Write($" -> {DifferenceMaxMin(array)}");
+
+
